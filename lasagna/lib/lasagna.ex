f9511f10501c33a12f15036c88bc3feeb209e2c6
PhilipeeX <dev.philipe@outlook.com>
2024-11-28 21:09:56 -0300

    feat(Lasagna): Finishes Lasagna exercise with all the tests passing.

---
+++ lasagna/lib/lasagna.ex
@@ -0,0 +1,23 @@
+defmodule Lasagna do
+  @minutes_per_layer 2
+
+  def expected_minutes_in_oven do
+    40
+  end
+
+  def remaining_minutes_in_oven(minutes) do
+    expected_minutes_in_oven - minutes
+  end
+
+  def preparation_time_in_minutes(layers) do
+    @minutes_per_layer * layers
+  end
+
+  def total_time_in_minutes(layers, minutes_in_the_oven) do
+    preparation_time_in_minutes(layers) + minutes_in_the_oven
+  end
+
+  def alarm do
+    "Ding!"
+  end
+end
