d2a6843db77c589d971ab5eb1996069287b6301e
PhilipeeX <dev.philipe@outlook.com>
2024-12-03 21:53:23 -0300

    feat(Language List): Add functions to create a new list and append elements to an existing one

---
+++ language-list/lib/language_list.ex
@@ -0,0 +1,25 @@
+defmodule LanguageList do
+  def new() do
+    []
+  end
+
+  def add(list, language) do
+    [language | list]
+  end
+
+  def remove(list) do
+
+  end
+
+  def first(list) do
+    # Please implement the first/1 function
+  end
+
+  def count(list) do
+    # Please implement the count/1 function
+  end
+
+  def functional_list?(list) do
+    # Please implement the functional_list?/1 function
+  end
+end
