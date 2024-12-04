cf06d5d9262bb6123fe181d47827b44aa165da42
PhilipeeX <dev.philipe@outlook.com>
2024-12-03 21:03:07 -0300

    refactor(LogLevel): Replace cond with case and use underscores for irrelevant legacy param

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,23 +1,23 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    cond do
-      level == 0 && !legacy? -> :trace
-      level == 1 -> :debug
-      level == 2 -> :info
-      level == 3 -> :warning
-      level == 4 -> :error
-      level == 5 && !legacy? -> :fatal
-      true -> :unknown
+    case {level, legacy?} do
+      {0, false} -> :trace
+      {1, _} -> :debug
+      {2, _} -> :info
+      {3, _} -> :warning
+      {4, _} -> :error
+      {5, false} -> :fatal
+      _ -> :unknown
     end
   end
 
   def alert_recipient(level, legacy?) do
-    label = to_label level, legacy?
-    cond do
-      label == :error || label == :fatal -> :ops
-      legacy? && label == :unknown -> :dev1
-      label == :unknown -> :dev2
-      true -> false
+    case to_label(level, legacy?) do
+      :error -> :ops
+      :fatal -> :ops
+      :unknown when legacy? -> :dev1
+      :unknown -> :dev2
+      _ -> false
     end
   end
 end
