500564fd6540e2cc9cf0de3a2238e8544c896768
PhilipeeX <dev.philipe@outlook.com>
2024-12-05 08:52:51 -0300

    wip(kitchen_calculator): declares first function to extract numeric value from tuples

---
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -0,0 +1,18 @@
+defmodule KitchenCalculator do
+  def get_volume(volume_pair) do
+    {_, numeric_component} = volume_pair
+    numeric_component
+  end
+
+  def to_milliliter(volume_pair) do
+    # Please implement the to_milliliter/1 functions
+  end
+
+  def from_milliliter(volume_pair, unit) do
+    # Please implement the from_milliliter/2 functions
+  end
+
+  def convert(volume_pair, unit) do
+    # Please implement the convert/2 function
+  end
+end
