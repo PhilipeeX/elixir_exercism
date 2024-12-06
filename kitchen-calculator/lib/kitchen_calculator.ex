28206930c2b59b0940fc5cb951784df4a7f737e9
PhilipeX <dev.philipe@outlook.com>
2024-12-05 21:55:42 -0300

    wip(kitchen_calculator): implements to_milliliter and from_milliliter functions using pattern matching

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,16 +1,22 @@
 defmodule KitchenCalculator do
+
   def get_volume(volume_pair) do
     {_, numeric_component} = volume_pair
     numeric_component
   end
 
-  def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
-  end
+  def to_milliliter({:milliliter, volume}), do: {:milliliter, volume}
+  def to_milliliter({:cup, volume}), do: {:milliliter, volume * 240}
+  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * 30}
+  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * 5}
+  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * 15}
+  def to_milliliter(_), do: :error
 
-  def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
-  end
+  def from_milliliter({:milliliter, milliliters}, :milliliter), do: {:milliliter, milliliters}
+  def from_milliliter({:milliliter, milliliters}, :cup), do: {:cup, milliliters / 240}
+  def from_milliliter({:milliliter, milliliters}, :fluid_ounce), do: {:fluid_ounce, milliliters / 30}
+  def from_milliliter({:milliliter, milliliters}, :teaspoon), do: {:teaspoon, milliliters / 5}
+  def from_milliliter({:milliliter, milliliters}, :tablespoon), do: {:tablespoon, milliliters / 15}
 
   def convert(volume_pair, unit) do
     # Please implement the convert/2 function
